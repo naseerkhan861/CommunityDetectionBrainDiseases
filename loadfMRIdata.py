8a10e23543749ded0b04051cdd91875945391a66
naseer <naseerkhan861@hotmail.com>
2019-04-08 14:51:56 +0800

    Add loadData functionality that reads tab delimited files of 1D format

--- loadfMRIdata.py
+++ loadfMRIdata.py
@@ -1,12 +1,34 @@
+import os
 import  numpy as np
 import matplotlib.pyplot as plt
-import os
+import  pandas as pd
 
 
-
+'function to joing root path and folder path where raw data is'
 def joinPath(rootPath,fileName):
     completePath = rootPathOfData + "\\" + regions_200
     return completePath
+
+'function to list all data files absolute path'
+def getAllDataFilesPath(filePath):
+    files=[]
+    for file in os.listdir(filePath):
+        files.append(filePath+"\\"+file)
+    return files
+
+'function to read a subject data that is in 1D format'
+def readFileData(path):
+    timeRegions=[]
+    with open(path,'r') as data:
+        for line in data:
+            values=line.strip().split("\t")
+            regions=[]
+            for val in values:
+                if val!="":
+                    regions.append(val)
+            timeRegions.append(regions)
+    return timeRegions
+
 
 
 
@@ -17,8 +39,14 @@ regions_200='rois_cc200'
 completePath=joinPath(rootPathOfData,regions_200)
 
 'listing all files in directory with absoulte path'
-for file in os.listdir(completePath):
-    print(completePath+"\\"+file)
+
+dataFilesPath=getAllDataFilesPath(completePath)
+
+subjectData=readFileData(dataFilesPath[0])
+
+for time_points in subjectData:
+    print("Length is:",len(time_points))
+
 
 
 
